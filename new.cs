3dee7a023c5bd3d5f3003183d46b48f76bb8a683
Abdullah <163624400+abdullah2309@users.noreply.github.com>
2026-07-31 11:59:58 +0500

    Fix syntax errors in new.cs

--- new.cs
+++ new.cs
@@ -1,4 +1,5 @@
 ///My name is a  Ali ///
 console.log("testing");
 console.log("testing2");
-let data = [23,45,2,3,254,34,33,32]
+let data = [23,45,2,3,254,34,33,32];
+let data = [23,45,2,3,254,34,33,32];
