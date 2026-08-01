6968f53f68059986cbefd7ce0f1dbe094c6b236f
Abdullah <163624400+abdullah2309@users.noreply.github.com>
2026-08-01 10:23:18 +0500

    Fix variable names for consistency
    Renamed variables for better readability.

--- new.cs
+++ new.cs
@@ -3,8 +3,8 @@ console.log("testing");
 console.log("testing2");
 let data = [23,45,2,3,254,34,33,32];
 let data2 = [22,53,45,23];
-let fisrtname = "M";
-let name = "saad";
+let fisrt_name = "M.";
+let last_name = "Saad";
 let age = 19;
 let address = "karachi";
 if(data){
